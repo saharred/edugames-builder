f15c798663ff31ed5f5c2af38b912aae70ebfa1a
saharred <saharco82@gmail.com>
2025-10-18 11:12:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
- base: '/edugames-builder/'
+  plugins: [react(), tsconfigPaths()],
+  base: '/edugames-builder/', // لو غيرتي اسم الريبو غيّري السطر ده يطابقه
 })
